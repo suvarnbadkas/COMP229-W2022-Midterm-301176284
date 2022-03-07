494a2e3d1880ae0853d8202ae7f2fe625f42c643
suvarn badkas <badkassuvarn@gmail.com>
2022-03-06 20:27:08 -0500

    some edits and added comments

--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,10 @@
+/*
+ File name: COMP229-W2022-MidTerm-301176284
+ Author: Suvarn Badkas
+ Student ID: 301176284  
+ Web app nmae: https://comp229w2022-midterm-301176284.herokuapp.com/
+ */
+
 module.exports = {
   //local MongoDB deployment ->
   //"URI": "mongodb://localhost/books229"
--- server/models/books.js
+++ server/models/books.js
@@ -1,3 +1,10 @@
+/*
+ File name: COMP229-W2022-MidTerm-301176284
+ Author: Suvarn Badkas
+ Student ID: 301176284  
+ Web app nmae: https://comp229w2022-midterm-301176284.herokuapp.com/
+ */
+
 let mongoose = require('mongoose');
 
 // create a model class
