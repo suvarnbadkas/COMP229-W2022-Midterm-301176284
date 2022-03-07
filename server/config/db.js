a5cf60c6ba31cb6796d6509103284c84fdbac8a7
suvarn badkas <badkassuvarn@gmail.com>
2022-03-06 20:04:35 -0500

    connected to mongo atlas with Uri

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
   //local MongoDB deployment ->
-  "URI": "mongodb://localhost/books229"
-  
+  //"URI": "mongodb://localhost/books229"
+  "URI": "mongodb+srv://admin:admin@dbserver.8tbxg.mongodb.net/booklist?retryWrites=true&w=majority"
 };
